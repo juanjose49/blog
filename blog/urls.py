8537dc55c348dc7ce1f62ef5370437693669e747
Juan Emeterio <juanjser@gmail.com>
2014-12-29 11:59:46 -0500

    Created deployment tool folder

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'content.views.home_page', name='home'),
     url(r'^content/', include('content.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
